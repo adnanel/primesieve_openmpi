c05557f2932da919c1da8ec46817fb34aa4a33ca
adnanel <adnanel94@gmail.com>
2020-09-25 01:41:10 +0200

    sieve

--- main.cpp
+++ main.cpp
@@ -12,11 +12,6 @@ int numResults(int min, int max) {
 
 /*
 int main(int argc, char** argv) {
-    int size = 2E9 / 4;
-    int sum = 0;
-    for (int i = 0; i <= 2E9 - size; i += size) {
-        sum += numResults(i, i + size);
-    }
 
     std::cout << sum;
 
@@ -34,6 +29,21 @@ int main(int argc, char *argv[]) {
 
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    printf("Hello, World from rank %d out of %d\n", rank, numtasks);
+
+    int range[2];
+    int size = 2E9 / 4;
+    int sum = 0;
+    for (int i = 0; i <= 2E9 - size; i += size) {
+        range[0] = i;
+        range[1] = i + size;
+
+        if (rank == 0) {
+            MPI_Send(range, 2, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        } else {
+            MPI_Recv(range, 2, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            std::cout << rank << ": I'm looking for primes in the range " << range[0] << " and " << range[1] << std::endl;
+        }
+    }
+
     MPI_Finalize();
 }
